702a8e999bd4694e4ccda385bd0a615d1d5b14b6
LiseAndreasen <lise.andreasen.1967@gmail.com>
2026-07-07 12:56:04 +0200

    part 2

--- 2026/d02.py
+++ 2026/d02.py
@@ -48,5 +48,25 @@ print("Part 1:", max_wall * max_pos)
 ###########################################################################
 # part 2
 
+wall = np.zeros_like(range(last_wall))
+robot_pos = 0
+wall_pos = 0
+wall_hit = 0
+
+for i in range(no_of_moves):
+	j = no_of_moves - i - 1
+	if moves[i] == ">":
+		robot_pos = (robot_pos + 1) % last_wall
+	if moves[i] == "<":
+		robot_pos = (robot_pos - 1) % last_wall
+	if moves[j] == ">":
+		wall_pos = (wall_pos + 1) % last_wall
+	if moves[j] == "<":
+		wall_pos = (wall_pos - 1) % last_wall
+	if robot_pos == wall_pos:
+		wall_hit += 1
+
+print("Part 2:", wall_hit)
+
 ###########################################################################
 # part 3
