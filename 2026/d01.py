d5e28a3214dc9c67f6d28b9d3cb674b6b306ee78
LiseAndreasen <lise.andreasen.1967@gmail.com>
2026-07-07 12:19:43 +0200

    part 2

--- 2026/d01.py
+++ 2026/d01.py
@@ -5,6 +5,8 @@
 # constants
 
 drinkable = 60
+heat_up_time = 1
+cool_down_time = 5
 input1 = "input_1_1.txt"
 
 ###########################################################################
@@ -18,12 +20,11 @@ def get_input():
     return lines
 
 ###########################################################################
-# 
+# part 1
 
 file = input1
 lines = get_input()
 lines = list(map(int, lines))
-#print(lines)
 
 sum_of_heatings = 0
 for temp in lines:
@@ -31,4 +32,17 @@ for temp in lines:
 		sum_of_heatings += drinkable - temp
 
 print(sum_of_heatings)
+
+###########################################################################
+# part 2
+
+sum_of_changes = 0
+for temp in lines:
+	if temp < drinkable:
+		sum_of_changes += (drinkable - temp) * heat_up_time
+	if drinkable < temp:
+		sum_of_changes += (temp - drinkable) * cool_down_time
+
+print(sum_of_changes)
+
 
