a098befced704ff815fdbb71981983d8a8fcc7f4
LiseAndreasen <lise.andreasen.1967@gmail.com>
2026-07-07 12:55:55 +0200

    part 1

---
+++ 2026/d02.py
@@ -0,0 +1,52 @@
+###########################################################################
+# import
+
+import numpy as np
+
+###########################################################################
+# constants
+
+input1 = "input_d02.txt"
+
+###########################################################################
+# functions
+
+def get_input():
+    lines = []
+    with open(file) as f:
+        for line in f:
+            lines.append(line.rstrip())
+    return lines
+
+###########################################################################
+# prep
+
+file = input1
+lines = get_input()
+moves = lines[0]
+
+###########################################################################
+# part 1
+
+# actual wall pos is 0-99
+last_wall = 100
+wall = np.zeros_like(range(last_wall))
+robot_pos = 0
+
+no_of_moves = len(moves)
+for i in range(no_of_moves):
+	if moves[i] == ">":
+		robot_pos = (robot_pos + 1) % last_wall
+	if moves[i] == "<":
+		robot_pos = (robot_pos - 1) % last_wall
+	wall[robot_pos] += 1
+
+max_wall = np.max(wall)
+max_pos = np.argmax(wall) + 1		# adjust wall pos, now 1-100
+print("Part 1:", max_wall * max_pos)
+
+###########################################################################
+# part 2
+
+###########################################################################
+# part 3
