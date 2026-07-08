18f5d3ffd7111aaf16406baf70d629d271259b12
LiseAndreasen <lise.andreasen.1967@gmail.com>
2026-07-08 12:37:04 +0200

    part 2

--- 2026/d03.py
+++ 2026/d03.py
@@ -4,7 +4,7 @@
 ###########################################################################
 # constants
 
-input1 = "input_d03_tst.txt"
+input1 = "input_d03.txt"
 
 ###########################################################################
 # functions
@@ -45,6 +45,49 @@ print("Part 1:", max_pwd)
 
 ###########################################################################
 # part 2
+
+max_score = 0
+max_pwd = ""
+
+for pwd in lines:
+	score = 0
+	if any(c.islower() for c in pwd):
+		score += 1
+	if any(c.isupper() for c in pwd):
+		score += 1
+	if any(c.isdigit() for c in pwd):
+		score += 1
+		
+	digit_seen = []
+	for c in pwd:
+		if c.isdigit():
+			if c not in digit_seen:
+				digit_seen.append(c)
+	if len(digit_seen) == 1 and digit_seen[0] == "7":
+		score += 7
+	
+	prev = ""
+	max_prev_no = 1
+	for c in pwd:
+		if c == prev:
+			prev_no += 1
+		else:
+			prev_no = 1
+		prev = c
+		if max_prev_no < prev_no:
+			max_prev_no = prev_no
+	if 3 <= max_prev_no:
+		score += max_prev_no * max_prev_no
+	
+	if "red" in pwd or "green" in pwd or "blue" in pwd:
+		score *= 3
+	
+	score *= len(pwd)
+	if max_score < score:
+		max_score = score
+		max_pwd = pwd
+
+print("Part 2:", max_pwd)
 
 ###########################################################################
 # part 3
