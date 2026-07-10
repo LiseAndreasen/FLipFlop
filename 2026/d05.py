9c9224eeac07f64887fa598ff87ec8728136088f
LiseAndreasen <lise.andreasen.1967@gmail.com>
2026-07-10 11:08:28 +0200

    all 3 parts

--- 2026/d05.py
+++ 2026/d05.py
@@ -18,54 +18,76 @@ def get_input():
             lines.append(line.rstrip())
     return lines
 
-def count_distinct(lines):
+def count_distinct(lines, illegal):
+	orglines = copy.deepcopy(lines)
+	
 	x = 0
 	y = 0
+	if illegal:
+		illegal_moves = 3
+	else:
+		illegal_moves = 0
 	distinct = 0
-	while(lines[y][x] != "*"):
+	
+	while(lines[y][x] != "*" or 0 < illegal_moves):
 		newx = x
 		newy = y
-		match lines[y][x]:
-			case ">":
-				newx += 1
-			case "<":
-				newx -= 1
-			case "v":
-				newy += 1
-			case "^":
-				newy -= 1
+		if lines[y][x] == "*":
+			if 0 < x < xmax and 0 < y < ymax:
+				match orglines[y][x]:
+					case ">":
+						newy += 1
+					case "<":
+						newy -= 1
+					case "v":
+						newx -= 1
+					case "^":
+						newx += 1
+				illegal_moves -= 1
+				d = 0
+			else:
+				illegal_moves = 0		# stop moving
+				d = 0
+		else:			
+			match lines[y][x]:
+				case ">":
+					newx += 1
+				case "<":
+					newx -= 1
+				case "v":
+					newy += 1
+				case "^":
+					newy -= 1
+			d = 1
 		lines[y][x] = "*"
 		x = newx
 		y = newy
-		distinct += 1
+		distinct += d
 	return distinct
 
 ###########################################################################
 # prep
 
 file = input1
-
-###########################################################################
-# part 1
-
 lines = get_input()
 lines2 = []
 for l in lines:
 	lines2.append(list(l))
-lines = lines2
 
-distinct = count_distinct(lines)
+###########################################################################
+# part 1
+
+lines = copy.deepcopy(lines2)
+illegal = False
+
+distinct = count_distinct(lines, illegal)
 
 print("Part 1:", distinct)
 
 ###########################################################################
 # part 2
 
-lines = get_input()
-lines2 = []
-for l in lines:
-	lines2.append(list(l))
-lines = lines2
+illegal = False
 
 xmax = len(lines[0]) - 1
 ymax = len(lines) - 1
@@ -74,9 +96,9 @@ distinct_max = 0
 for i in range(1, xmax):
 	for j in range(1, ymax):
 		for k in "<>v^":
-			newlines = copy.deepcopy(lines)
+			newlines = copy.deepcopy(lines2)
 			newlines[j][i] = k
-			distinct = count_distinct(newlines)
+			distinct = count_distinct(newlines, illegal)
 			if distinct_max < distinct:
 				distinct_max = distinct
 
@@ -85,4 +107,19 @@ print("Part 2:", distinct_max)
 ###########################################################################
 # part 3
 
-print("Part 3:")
+illegal = True
+
+xmax = len(lines[0]) - 1
+ymax = len(lines) - 1
+distinct_max = 0
+
+for i in range(1, xmax):
+	for j in range(1, ymax):
+		for k in "<>v^":
+			newlines = copy.deepcopy(lines2)
+			newlines[j][i] = k
+			distinct = count_distinct(newlines, illegal)
+			if distinct_max < distinct:
+				distinct_max = distinct
+
+print("Part 3:", distinct_max)
