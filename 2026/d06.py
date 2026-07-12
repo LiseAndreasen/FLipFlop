4bf1fa5423ef2dc77e3f505825a054f2dda5dd5f
LiseAndreasen <lise.andreasen.1967@gmail.com>
2026-07-12 02:38:58 +0200

    all 3 parts

--- 2026/d06.py
+++ 2026/d06.py
@@ -11,6 +11,10 @@ import re
 input1 = "input_d06_tst.txt"
 input2 = "input_d06.txt"
 dirs = [[-1, 0], [0, -1], [0, 1], [1, 0]]
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
+	61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137,
+	139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211,
+	223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293]
 
 ###########################################################################
 # functions
@@ -32,6 +36,30 @@ def print_map(map):
 	for m in map:
 		print("".join(m))
 
+# count number of gears in group next to coordinates
+# bluetooth output
+def count_group(map, r, c):
+	local_map = copy.deepcopy(map)
+	cell1 = [r, c]
+	cells = [cell1]
+	no_of_gears = 0
+
+	while 0 < len(cells):
+		cell = cells.pop(0)
+		[rl, cl] = cell
+		gear = local_map[rl][cl]
+		if gear == "3" or gear.isupper():
+			if gear == "3":
+				no_of_gears += 1
+			for dir in dirs:
+				[dr, dc] = dir
+				char = get_value(local_map, rl + dr, cl + dc)
+				if char == "3":
+					cells.append([rl+dr, cl+dc])
+			local_map[rl][cl] = "*"
+	
+	return no_of_gears
+
 # returns _ when the coordinates are outside the map
 def get_value(map, r, c):
 	if r < 0 or c < 0:
@@ -48,10 +76,17 @@ def turn_gears(map, part):
 			all_gears = "#"
 			all_lights = "*"
 			all_bluetooth = ""
+			prime_rule = False
 		case 2:
 			all_gears = "#3"
 			all_lights = "*"
 			all_bluetooth = "abcdefghijklmnopqrstuvwxyz"
+			prime_rule = False
+		case 3:
+			all_gears = "#3"
+			all_lights = "*"
+			all_bluetooth = "abcdefghijklmnopqrstuvwxyz"
+			prime_rule = True
 	
 	turns = copy.deepcopy(map)
 	cell1 = find_char("S", turns)
@@ -84,6 +119,10 @@ def turn_gears(map, part):
 				upper_char = char.upper()
 				upper_cell = find_char(upper_char, map)
 				[ru, cu] = upper_cell
+				if prime_rule:
+					gear_count = count_group(map, ru, cu)
+					if gear_count in primes:
+						continue
 				if turn == "R":
 					turns[r+dr][c+dc] = "L"
 					turns[ru][cu] = "R"
@@ -129,4 +168,6 @@ print("Part 2:", final_state)
 ###########################################################################
 # part 3
 
-print("Part 3:")
+part = 3
+final_state = turn_gears(map, part)
+print("Part 3:", final_state)
