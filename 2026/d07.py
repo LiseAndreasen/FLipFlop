5e82da582bea757ecd8270c9cab52761dcbccb49
LiseAndreasen <lise.andreasen.1967@gmail.com>
2026-07-12 16:56:43 +0200

    all 3 parts

--- 2026/d07.py
+++ 2026/d07.py
@@ -19,6 +19,56 @@ def get_input():
             lines.append(line.rstrip())
     return lines
 
+def snake_walk(moves, sushi, part):
+	r = 0
+	c = 0
+	sushi_no = 0
+	next_sushi = re.findall('[0-9]+', sushi[sushi_no])
+	[nsc, nsr] = next_sushi
+	snake = [[r,c]]
+	eaten = 0
+	if part == 3:
+		moves_no = len(moves)
+	else:
+		moves_no = int(len(moves)/2)
+	
+	for i in range(moves_no):
+		match moves[i]:
+			case ">":
+				c += 1
+			case "<":
+				c -= 1
+			case "^":
+				r += 1
+			case "v":
+				r -= 1
+				
+		if int(nsr) == r and int(nsc) == c:
+			sushi_no += 1
+			if sushi_no < len(sushi):
+				next_sushi = re.findall('[0-9]+', sushi[sushi_no])
+				[nsc, nsr] = next_sushi
+			else:
+				[nsc, nsr] = [-1,-1]
+		else:
+			# snake won't grow
+			snake.pop(0)
+		if part == 2:
+			if [r,c] in snake:
+				# snake dies
+				return [sushi_no, len(snake), eaten * len(snake)]
+		if part == 3:
+			if [r,c] in snake:
+				# snake eats segment, rest falls off
+				while [r,c] in snake:
+					snake.pop(0)
+				# then remove 1 more segment
+				snake.pop(0)
+				eaten += 1
+		snake.append([r,c])
+	
+	return [sushi_no, len(snake), eaten * len(snake)]
+
 ###########################################################################
 # prep
 
@@ -32,34 +82,20 @@ sushi = lines
 ###########################################################################
 # part 1
 
-r = 0
-c = 0
-sushi_no = 0
-next_sushi = re.findall('[0-9]+', sushi[sushi_no])
-[nsc, nsr] = next_sushi
-for i in range(int(len(moves)/2)):
-	match moves[i]:
-		case ">":
-			c += 1
-		case "<":
-			c -= 1
-		case "^":
-			r += 1
-		case "v":
-			r -= 1
-	if int(nsr) == r and int(nsc) == c:
-		sushi_no += 1
-		next_sushi = re.findall('[0-9]+', sushi[sushi_no])
-		[nsc, nsr] = next_sushi
-
+part = 1
+[sushi_no, length, eaten] = snake_walk(moves, sushi, part)
 print("Part 1:", sushi_no)
 
 ###########################################################################
 # part 2
 
-print("Part 2:")
+part = 2
+[sushi_no, length, eaten] = snake_walk(moves, sushi, part)
+print("Part 2:", length)
 
 ###########################################################################
 # part 3
 
-print("Part 3:")
+part = 3
+[sushi_no, length, eaten] = snake_walk(moves, sushi, part)
+print("Part 3:", eaten)
